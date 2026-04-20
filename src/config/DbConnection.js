2d1ed0dca5753fef9088dee7d56fbe0718ea2f10
Soumyaditya Mondal <souhttp@gmail.com>
2026-04-20 10:58:30 +0530

    Db max pool connection

--- src/config/DbConnection.js
+++ src/config/DbConnection.js
@@ -2,7 +2,21 @@ const mongoose=require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}`);
+
+    const options = {
+      // maintain up to 200 socket connections (Default is 10)
+      maxPoolSize: 400, 
+      // keep at least 10 connections open
+      minPoolSize: 10,  
+      // how long the MongoDB driver will wait before timing out a query
+      socketTimeoutMS: 45000, 
+      // helps with connectivity on slow free-tier networks
+      serverSelectionTimeoutMS: 5000,
+      // wait time for a connection to become available from the pool
+      waitQueueTimeoutMS: 5000, 
+    };
+
+    await mongoose.connect(`${process.env.MONGO_URI}`, options);
     console.log('Connected MongoDb');
   } catch (err) {
     console.error(err);
